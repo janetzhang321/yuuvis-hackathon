1fd2b8621807393121987a091b8c995d5d5782ad
Janet Zhang <janetzhang321@gmail.com>
2019-04-06 16:47:49 -0400

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['POST','GET'])
 def search():
     return render_template("index.html")
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,18 @@
 document.getElementById('my-form').addEventListener("submit", function (e) {
     e.preventDefault();
     let searchResult = document.querySelector(".search-bar").value;
+    console.log(searchResult)
     return (searchResult)
 });
+
+let query={
+    "query": {
+      "statement": "SELECT * FROM enaio:object where contains(`'${searchResult}'`)",
+      "skipCount": 0,
+      "maxItems": 50
+    }
+  }
+
+let myJSON = JSON.stringify(query);
+
+console.log(myJSON)
