d47db2e1f4f0287c924cc213ddfd7f3a7ccffd7d
Janet Zhang <janetzhang321@gmail.com>
2019-04-06 18:40:42 -0400

    requests working

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 from flask import Flask, render_template, request, url_for
+import urllib
+import json
+from http.client import HTTPSConnection
 app = Flask(__name__)
+
+headers = {
+    'Authorization':"Basic YWRtaW46TGYwYll4dGo4UTdt",
+    'X-ID-TENANT-NAME':"nyc093",
+    'Content-Type':'application/json',
+    'Accept':'application/json'
+}
+
+#This sets up the https connection
+c = HTTPSConnection("yuuvis.io")
+
+def build(word):
+    host = "/api/dms/objects/search"
+    payload = {
+        "query": {
+                "statement": "SELECT * FROM enaio:object where contains('%s')"%word,
+                "skipCount": 0,
+                "maxItems": 50
+            }
+    }
+    json_load = json.dumps(payload)
+
+    c.request('POST', host, json_load, headers = headers)
+    #get the response back
+    res = c.getresponse()
+    # at this point you could check the status etc
+    # this gets the page text
+    data = res.read()
+    print(data)
 
 @app.route("/", methods=['POST','GET'])
 def search():
+    build('janet')
     return render_template("index.html")
 
 @app.route("/display", methods=['POST','GET'])
 def display():
     word = request.form.get('search')
     print(word)
+    build(word)
     return render_template("display.html", word=word)
 
 if __name__ == '__main__':
