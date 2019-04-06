09fffcfddfda11019d8a03f53f20e68357f0642d
Janet Zhang <janetzhang321@gmail.com>
2019-04-06 16:56:52 -0400

    json query

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,18 @@
 document.getElementById('my-form').addEventListener("submit", function (e) {
     e.preventDefault();
     let searchResult = document.querySelector(".search-bar").value;
-    console.log(searchResult)
-    return (searchResult)
-});
+    //console.log(searchResult)
 
-let query={
-    "query": {
-      "statement": "SELECT * FROM enaio:object where contains(`'${searchResult}'`)",
-      "skipCount": 0,
-      "maxItems": 50
+    let query={
+        "query": {
+        "statement": "SELECT * FROM enaio:object where contains('"+searchResult+"')",
+        "skipCount": 0,
+        "maxItems": 50
+        }
     }
-  }
 
-let myJSON = JSON.stringify(query);
+    let myJSON = JSON.stringify(query);
 
-console.log(myJSON)
+    console.log(myJSON)
+    return (myJSON)
+});
